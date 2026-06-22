1e0121163cea5e36837346fed6238abaaf388e9a
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-06-21 21:30:22 -0500

    make test for reverse() pass

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,11 @@
-import { capitalize } from './index.js';
+import { capitalize, reverse } from './index.js';
 
 test('check capitalization', () => {
     expect(capitalize('hello')).toBe('Hello');
     expect(capitalize('world')).toBe('World');
+});
+
+test('reverse string', () => {
+    expect(reverse('hello')).toBe('olleh');
+    expect(reverse('world')).toBe('dlrow');
 });
