9b3b6fc7e0758d0723e44321eafd6745fd45926b
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-06-21 20:50:16 -0500

    fix eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,19 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import pluginJest from "eslint-plugin-jest";
 
-export default defineConfig([
+export default([
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    // update this to match your test files
+    files: ["src/index.js", "src/index-spec.js"],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
+    },
   },
 ]);
---
+++ src/index-spec.js
@@ -0,0 +1,4 @@
+test('check capitalization', () => {
+    expect(capitalize('hello')).toBe('Hello');
+    expect(capitalize('world')).toBe('World');
+});
